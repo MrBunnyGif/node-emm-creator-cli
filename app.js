94288e595a5d52344a3a738d4d95968a0a7aee90
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-01-21 15:40:08 -0300

    Creates organized email html

--- app.js
+++ app.js
@@ -2,18 +2,34 @@ import fs from 'fs';
 import sizeOf from 'image-size';
 
 const files = fs.readdirSync('./src');
+const fileTitle = 'titulo do email'
 let emmRows = []
+let rowWithColumns = []
 
 files.forEach(file => {
 	const dimensions = sizeOf(`./src/${file}`);
-	emmRows.push(
-		`<tr><td><img style="display: block; border: 0;" align="top" width="${dimensions.width}" height="${dimensions.height}"	src="/src/${file}" alt="Mundo lenovo" /></td></tr>`
-	)
+	if (dimensions.width < 600)
+		rowWithColumns.push(
+			`<td><img style="display: block; border: 0;" align="top" width="${dimensions.width}" height="${dimensions.height}"	src="/src/${file}" alt="${fileTitle}" /></td>`
+		)
+	else if (dimensions.width >= 600 && rowWithColumns.length) {
+		emmRows.push(
+			`<tr><td><table style="margin: auto;" border="0" cellspacing="0" cellpadding="0" bgcolor="#DDDDDD"><tr>${rowWithColumns.join('')}</td></table></td></tr>`
+		)
+		emmRows.push(
+			`<tr><td><img style="display: block; border: 0;" align="top" width="${dimensions.width}" height="${dimensions.height}"	src="/src/${file}" alt="${fileTitle}" /></td></tr>`
+		)
+		rowWithColumns = []
+	}
+	else
+		emmRows.push(
+			`<tr><td><img style="display: block; border: 0;" align="top" width="${dimensions.width}" height="${dimensions.height}"	src="/src/${file}" alt="${fileTitle}" /></td></tr>`
+		)
 })
+console.log('rowWithColumns: ', rowWithColumns.length)
 
-const htmlContent = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>ISG e pauta</title></head><body><table style="margin: auto;" border="0" cellspacing="0" cellpadding="0" bgcolor="#DDDDDD">${emmRows.join('')}</table></body></html>`
+const htmlContent = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>${fileTitle}</title></head><body><table style="margin: auto;" border="0" cellspacing="0" cellpadding="0" bgcolor="#DDDDDD">${emmRows.join('')}</table></body></html>`
 
 fs.writeFile('index.html', htmlContent, function (err) {
-  if (err) throw err;
-  console.log('Saved!');
+	if (err) throw err;
 }); 
