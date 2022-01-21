07c61be0bc2ee8ccbc9a8a1f76c57b632ec5b5c2
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-01-21 11:34:15 -0300

    Readinf image files

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const sizeOf = require('image-size');
 
-const dimensions = sizeOf('./tests/1557417594311.jpeg');
-const files = fs.readdirSync('./tests');
-console.log(dimensions.width, dimensions.height);
-console.log("🚀 ~ file: app.js ~ line 11 ~ files", files)
+const files = fs.readdirSync('./src');
+let html = []
+
+files.forEach(file => {
+	const dimensions = sizeOf(`./src/${file}`);
+	html.push(
+		`<tr><td><img style="display: block; border: 0;" align="top" width="${dimensions.width}" height="${dimensions.height}"	src="/src/${file}" alt="Mundo lenovo" /></td></tr>`
+		)
+	})
+	console.log("🚀 ~ file: app.js ~ line 6 ~ html", html)
