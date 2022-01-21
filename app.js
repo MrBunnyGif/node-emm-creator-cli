553f3a8b7d141757ff6c9f040394ad4c2ed9779d
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-01-21 11:41:52 -0300

    Updates app to be a module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const sizeOf = require('image-size');
+import fs from 'fs';
+import sizeOf from 'image-size';
+import copy from 'copy-to-clipboard';
 
 const files = fs.readdirSync('./src');
 let html = []
@@ -8,6 +9,7 @@ files.forEach(file => {
 	const dimensions = sizeOf(`./src/${file}`);
 	html.push(
 		`<tr><td><img style="display: block; border: 0;" align="top" width="${dimensions.width}" height="${dimensions.height}"	src="/src/${file}" alt="Mundo lenovo" /></td></tr>`
-		)
-	})
-	console.log("🚀 ~ file: app.js ~ line 6 ~ html", html)
+	)
+})
+console.log("🚀 ~ file: app.js ~ line 6 ~ html", html)
+// copy('oioi')
