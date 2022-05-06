84679130ad37f3c07cfb2a9d4890c47b67f9db72
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-05-06 09:40:27 -0300

    Fix head

--- front-scripts.js
+++ front-scripts.js
@@ -40,7 +40,7 @@ module.exports = () => {
 	}
 
 	function exportEmm() {
-		var aFileParts = [`<html><head></head><body><style type="text/css">body{margin:0;padding:0;background-color:#d0d4d7;border-spacing:0;border:0}*{border:0;padding:0}a{outline:0}td>div{line-height:0}div{display:block!important}</style>${document.querySelector('table').outerHTML}</body></html>`];
+		var aFileParts = [`<html><head><title>titulo do email</title><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /><meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"><style type="text/css">body{margin:0;padding:0;background-color:#d0d4d7;border-spacing:0;border:0}*{border:0;padding:0}a{outline:0}td>div{line-height:0}div{display:block!important}</style></head><body>${document.querySelector('table').outerHTML}</body></html>`];
 		let file = new Blob(aFileParts, { type: 'text/html' });
 		let a = document.createElement("a"),
 			url = URL.createObjectURL(file);
